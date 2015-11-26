fbf30d7f45b144aa9c7e0cf9b3527d6e97da0db3
Nio-av <public@nio-av.de>
2015-11-26 19:02:28 +0100

    Landing-Page - Improfements

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link href="css/basic.css" rel="stylesheet" type="text/css"></link>
+    <link href="css/index.css" rel="stylesheet" type="text/css"></link>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300' rel='stylesheet' type='text/css'>
+   
+	<title>Datenschutzumfrage</title>
+    
+</head>
+
+
+
+
+
+<body class="background">
+    <?php
+        include_once "res/basicsdk.php"
+    ?>
+    <script>
+    
+    
+    window.fbAsyncInit = function() {
+    FB.init({
+          appId      : '1519037951732644',
+          xfbml      : true,
+          version    : 'v2.5'
+        });
+      
+        
+            FB.getLoginStatus(function(response) {
+              if (response.status === 'connected') {
+                console.log('Logged in.');
+              }
+              else {
+                  console.log('Not Logged in.');
+                //FB.login();
+              }
+            });
+        
+    };
+    </script>
+    
+    
+    
+    <article>
+            <h1>Danke für deine Mithilfe!</h1>
+            <p>Bitte logge dich ein, um an der Umfrage teilzunehmen. :)</p>
+    </article>
+
+     
+
+    <div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true" scope="public_profile,user_friends,email,user_about_me,user_actions.books,user_actions.fitness,user_actions.music,user_actions.news,user_actions.video,user_birthday,user_education_history,user_events,user_games_activity,user_hometown,user_likes,user_location,user_managed_groups,user_photos,user_posts,user_relationships,user_relationship_details,user_religion_politics,user_tagged_places,user_videos,user_website,user_work_history,read_custom_friendlists,read_insights,read_audience_network_insights,read_page_mailboxes,manage_pages,publish_pages,rsvp_event,pages_show_list,pages_manage_cta,pages_manage_leads,ads_read,ads_management" onlogin="goon()"></div>
+    
+    <script>
+    function goon(){
+        window.location.href = "umfrage.php";
+        document.getElementById("GoOn-Link").style.display = 'table-row';
+    }
+        
+        
+
+    </script>
+    
+    <div id="GoOn-Link">
+        <a href="umfrage.php">Weiter zur Umfrage.</a>
+    </div>
+
+    <!--
+    <div id="Like-Button">
+        <article>
+          <div
+            class="fb-like"
+            data-share="true"
+            data-width="450"
+            data-show-faces="true">
+          </div>
+        </article>
+    </div>
+    -->
+    
+    
+    <?php
+            include_once "res/footer.php";
+    ?>
+    
+</body>
+
+</html>
