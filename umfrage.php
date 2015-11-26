33f586d109f8118c88a56d6fca405509c8bb6168
Nio-av <public@nio-av.de>
2015-11-26 19:02:58 +0100

    Included Fake-Survey

---
+++ umfrage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link href="css/basic.css" rel="stylesheet" type="text/css"></link>
+    <link href="css/umfrage.css" rel="stylesheet" type="text/css"></link>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300' rel='stylesheet' type='text/css'>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
+
+    <title>Datenschutzumfrage</title>
+
+</head>
+
+
+
+
+
+<body class="background">
+    <article>
+        <h1>Wie wichtig ist dir Datenschutz?</h1>
+        <div class="btn-group btn-group-justified" role="group" aria-label="Justified button group">
+            <a href="#important" class="btn btn-default" role="button">Wichtig</a>
+            <a href="#unimportant" class="btn btn-default" role="button">Unwichtig</a>
+        </div>
+        
+        <nav>
+            <a href="./">Zurück zur Startseite</a>
+            <a href="./auswertung.php">Zur Auswertung.</a>
+        </nav>
+    </article>
+
+    
+    <?php include_once "res/footer.php"; ?>
+
+</body>
+
+</html>
